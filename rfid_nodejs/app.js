13a12332ac0828cc1dd4da6fbaae16c25407f8cb
Nathaniel Steier <nsteier@gmail.com>
2013-11-02 17:50:26 -0400

    Added 'test' view

--- rfid_nodejs/app.js
+++ rfid_nodejs/app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var test = require('./routes/test');
 
 var app = express();
 
@@ -30,6 +31,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/test', test.test);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
