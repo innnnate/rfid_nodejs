fb39c4504b5378bac3ab36b28605015f485b577c
Nathaniel Steier <nsteier@gmail.com>
2013-11-02 22:26:29 -0400

    Commit sample authentication logic

--- rfid_nodejs/routes/api.js
+++ rfid_nodejs/routes/api.js
@@ -12,7 +12,51 @@ exports.testForm = function(req, res){
 
 exports.clientAPI = function(req, res){
 
-  res.json(req.body);
+  var STATUS_BAD_DATA = 'ERRDATA';
+  var STATUS_SUCCESS = 'SUCCESS';
+  var STATUS_FAILED_ID = 'AUTH_FAILED';
+  var STATUS_FAILED_CONTEXT = 'INVALID_CONTEXT';
+  var STATUS_FAILED_AUTHSTRING = 'INVALID_AUTHSTRING';
+  
 
+  var jsontext = req.body;
+  
+  var response = {
+            id: jsontext.id,  
+            context: jsontext.context,  
+            authString: jsontext.authString,  
+            statusCode: STATUS_BAD_DATA,
+			statusText: ""
+		};
+
+  if(jsontext.context != 'Door')
+  {
+	response.statusCode = STATUS_FAILED_CONTEXT;
+	response.statusText = "I'm not sure where you are?!";
+	res.json(response);
+	return;
+  }
+
+  if(jsontext.authString != 'l3tm3in123')
+  {
+	response.statusCode = STATUS_FAILED_AUTHSTRING;
+	response.statusText = "I don't recognize that string";
+	res.json(response);
+	return;
+  }
+
+  if(jsontext.id != 'Me')
+  {
+	response.statusCode = STATUS_FAILED_ID;
+	response.statusText = "Who you are?!";
+	res.json(response);
+	return;
+  }
+
+  //Otherwise, success
+  
+  response.statusCode = STATUS_SUCCESS;
+  response.statusText = "You are successful!!";
+  res.json(response);
+  
 };
-
