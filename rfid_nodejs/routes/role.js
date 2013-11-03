7ce0956b0cbfc4a7e19deb61b0d5f57edb3995c0
Nathaniel Steier <nsteier@gmail.com>
2013-11-03 17:49:53 -0500

    Added role.js

---
+++ rfid_nodejs/routes/role.js
@@ -0,0 +1,25 @@
+
+/*
+ * GET roles listing.
+ */
+
+var DB = require("../lib/db");
+
+ 
+exports.list = function(req, res){
+
+
+  DB.Role.find( {},'name' ,function (err, roleList) {
+    if (err) {
+	  console.log("err", err);
+	}
+	console.log(roleList);
+	
+    var response = {
+	  status: "OK",
+	  users: roleList
+	};
+	res.json( response );
+	return;
+  })
+};
