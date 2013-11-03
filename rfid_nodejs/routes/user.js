3ef57b6250510f6ca7b783e5e9b28ba955e9f737
Nathaniel Steier <nsteier@gmail.com>
2013-11-03 17:15:48 -0500

    Modified JSON response returned by GET /api/users

--- rfid_nodejs/routes/user.js
+++ rfid_nodejs/routes/user.js
@@ -8,19 +8,18 @@ var DB = require("../lib/db");
  
 exports.list = function(req, res){
 
-  var STATUS_SUCCESS = 'SUCCESS';
 
-  DB.User.find( function (err, userList) {
+  DB.User.find( {},'name id roles' ,function (err, userList) {
     if (err) {
 	  console.log("err", err);
 	}
 	console.log(userList);
 	
     var response = {
-	  statusCode: STATUS_SUCCESS,
-	  userList: JSON.stringify(userList)
+	  status: "OK",
+	  users: JSON.stringify(userList)
 	};
-	res.json(response);
+	res.json( response );
 	return;
   })
 };
